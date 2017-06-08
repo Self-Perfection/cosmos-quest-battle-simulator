ca1996d757e75f6fac3a0a88305ca33486b1a67c
Self-Perfection <alexander.s.m+git@gmail.com>
2017-06-08 14:52:50 +0300

    +png performance graph, skip missing python interpreters
    I want performance graph in README.

--- benchmark/update_performance_graph.sh
+++ benchmark/update_performance_graph.sh
@@ -6,20 +6,30 @@ BENCHMARK_DAT_FILE='current_performance.dat'
 : > "$BENCHMARK_DAT_FILE"
 
 for interpreter in 'python2' 'python3' 'pypy' 'pypy3'; do
+    if ! command -v "$interpreter" >/dev/null; then
+        echo "$interpreter not found. Skipping"
+        continue
+    fi
     version=$("$interpreter" --version 2>&1 | awk 'END{print $2}')
     printf '"%s %s"\t' "$interpreter" "$version" | tee -a "$BENCHMARK_DAT_FILE"
     /usr/bin/time --format='%U'  "$interpreter" $CMDLINE 3>&2 2>&1 1>/dev/null | tail -1 | tee -a "$BENCHMARK_DAT_FILE"
 done
 
 CPUMODEL=$(grep -m1 'model name' /proc/cpuinfo |cut -d: -f2)
-gnuplot << EOF
-set terminal svg
-set output "current_performance.svg"
+for t in svg png; do
+    gnuplot << EOF
+    set terminal $t
+    set output "current_performance.$t"
 
-set title "Running time of '$CMDLINE'\non${CPUMODEL} in seconds" noenhanced
-set style fill solid
-set boxwidth 0.8
-set yrange[0:]
+    set title "Running time of '$CMDLINE'\non${CPUMODEL} in seconds" noenhanced
+    set style fill solid
+    set boxwidth 0.8
+    set yrange[0:]
 
-plot "current_performance.dat" using 2:xtic(1) with boxes
+    plot "current_performance.dat" using 2:xtic(1) with boxes
 EOF
+done
+
+#Lossleless optimization of png compression
+optipng -i 0 -o7 current_performance.png
+advdef -z4 current_performance.png
